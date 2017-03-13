48506968ae0475e56d2fed2f20d30e73859e15f4
Evan Hopper-Moore <evanhoppermoore@gmail.com>
2017-03-13 15:50:40 -0700

    Remove position from converted string

--- src/convert.py
+++ src/convert.py
@@ -5,16 +5,19 @@ def listToString(customList):
     output = ''
 
     for note in customList:
-        output += str(note.pos) + ',' + str(note.len) + ',' + str(note.val) + ',' + str(note.vel) + ' '
+        output += str(note.len) + ',' + str(note.val) + ',' + str(note.vel) + ' '
 
     return output
 
 def stringToList(stringList):
     output = []
     splitOne = stringList.split(' ')
+    trackPos = 0
     for note in splitOne:
         splitString = note.split(',')
         if len(splitString) > 1 :
-            output.append(CustomNote(pos=int(splitString[0]), len=int(splitString[1]), val=int(splitString[2]), vel=int(splitString[3])))
+            output.append(CustomNote(pos=trackPos, len=int(splitString[0]), val=int(splitString[1]), vel=int(splitString[2])))
+            trackPos += int(splitString[0])
+            
 
     return output
